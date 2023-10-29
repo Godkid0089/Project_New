c013ac4be169ada01480c991ed0be95d0281df3b
Godkid0089 <143862337+Godkid0089@users.noreply.github.com>
2023-10-29 11:02:38 +0800

    m

---
+++ Game.js
@@ -0,0 +1,8 @@
+var pCount = document.querySelector(".pCount");
+var cCount = document.querySelector(".cCount");
+var pCount = document.querySelector(".pCount");
+var computer = document.querySelector(".computer");
+console.log(pCount);
+console.log(cCount);
+function play(pChoice){
+	var cChoice
